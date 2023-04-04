a3e983428ede762586926294221902139aba8998
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2023-04-04 14:20:52 +0800

    Initial Commit

---
+++ Qricko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qricko
+//
+//  Created by Yi-Chin Hsu on 2023/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
